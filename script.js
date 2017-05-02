77a54ed14d24f3ecda802e76217b45c95dac8558
Josh Rufin <joshrufin@gmail.com>
2017-05-02 07:31:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var app = angular.module('ourModule', []);
+
+app.controller('ourController', function($scope){
+
+      $scope.taskList = [];
+
+      $scope.addTask = function(newTask){
+
+      console.log(newTask);
+
+      $scope.taskList.push({ name: newTask.name });
+      console.log($scope.taskList);
+      }
+
+      $scope.remove = function(task) {
+      var index = $scope.taskList.indexOf(task);
+      $scope.taskList.splice($scope.task, 0);
+      }
+
+
+
+});
